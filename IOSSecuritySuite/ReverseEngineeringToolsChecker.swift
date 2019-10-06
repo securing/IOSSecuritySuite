93fa2adf9ad9c2a8971a4116ae307319b0964bca
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2019-10-06 12:27:46 -0700

    1) Make _dyld_get_image_name compare case insensitive 2) Fix SwiftLint Format

--- IOSSecuritySuite/ReverseEngineeringToolsChecker.swift
+++ IOSSecuritySuite/ReverseEngineeringToolsChecker.swift
@@ -10,91 +10,91 @@ import Foundation
 import MachO // dyld
 
 internal class ReverseEngineeringToolsChecker {
-    
+
     static func amIReverseEngineered() -> Bool {
         return (checkDYLD() || checkExistenceOfSuspiciousFiles() || checkOpenedPorts())
     }
-    
+
     private static func checkDYLD() -> Bool {
-        
+
         let suspiciousLibraries = [
             "FridaGadget",
             "frida", // Needle injects frida-somerandom.dylib
             "cynject",
             "libcycript"
         ]
-        
+
         for libraryIndex in 0..<_dyld_image_count() {
-            
+
             // _dyld_get_image_name returns const char * that needs to be casted to Swift String
             guard let loadedLibrary = String(validatingUTF8: _dyld_get_image_name(libraryIndex)) else { continue }
-            
+
             for suspiciousLibrary in suspiciousLibraries {
-                if loadedLibrary.contains(suspiciousLibrary) {
+                if loadedLibrary.lowercased().contains(suspiciousLibrary.lowercased()) {
                     return true
                 }
             }
         }
-        
+
         return false
     }
-    
+
     private static func checkExistenceOfSuspiciousFiles() -> Bool {
-        
+
         let paths = [
             "/usr/sbin/frida-server"
         ]
-        
+
         for path in paths {
             if FileManager.default.fileExists(atPath: path) {
                 return true
             }
         }
-        
+
         return false
     }
-    
+
     private static func checkOpenedPorts() -> Bool {
-        
+
         let ports = [
             27042, // default Frida
             4444 // default Needle
         ]
-        
+
         for port in ports {
-            
+
             if canOpenLocalConnection(port: port) {
                 return true
             }
         }
-        
+
         return false
     }
-    
+
     private static func canOpenLocalConnection(port: Int) -> Bool {
-        
+
         func swapBytesIfNeeded(port: in_port_t) -> in_port_t {
             let littleEndian = Int(OSHostByteOrder()) == OSLittleEndian
             return littleEndian ? _OSSwapInt16(port) : port
         }
-        
+
         var serverAddress = sockaddr_in()
         serverAddress.sin_family = sa_family_t(AF_INET)
         serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1")
         serverAddress.sin_port = swapBytesIfNeeded(port: in_port_t(port))
         let sock = socket(AF_INET, SOCK_STREAM, 0)
-        
+
         let result = withUnsafePointer(to: &serverAddress) {
             $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
                 connect(sock, $0, socklen_t(MemoryLayout<sockaddr_in>.stride))
             }
         }
-        
+
         if result != -1 {
             return true // Port is opened
         }
-        
+
         return false
     }
-    
+
 }
