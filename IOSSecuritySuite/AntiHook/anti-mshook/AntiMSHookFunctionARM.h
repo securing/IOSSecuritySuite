02919d3d0997058bdb44d34f62d52dce4a14251f
jintao <>
2020-04-22 13:54:47 +0800

    add antiHook

---
+++ IOSSecuritySuite/AntiHook/anti-mshook/AntiMSHookFunctionARM.h
@@ -0,0 +1,16 @@
+//
+//  AntiMSHook.h
+//  AntiMSHook
+//
+//  Created by jintao on 2019/9/17.
+//  Copyright © 2019 jintao. All rights reserved.
+//
+
+#ifndef AntiMSHookFunctionARM_h
+#define AntiMSHookFunctionARM_h
+
+#include <stdio.h>
+
+void* antiMSHook(void *orig_addr);
+
+#endif /* AntiMSHook_h */
---
+++ IOSSecuritySuite/AntiHook/anti-mshook/MSHookFunctionARMCheck.h
@@ -0,0 +1,16 @@
+//
+//  MSHookFunctionARMCheck.h
+//  AntiMSHook
+//
+//  Created by jintao on 2019/9/18.
+//  Copyright © 2019 jintao. All rights reserved.
+//
+
+#ifndef MSHookFunctionARMCheck_h
+#define MSHookFunctionARMCheck_h
+
+#include <stdio.h>
+
+_Bool MSHookARMCheck(void *symbol_addr);
+
+#endif /* MSHookFunctionARMCheck_h */
