319e9a00340cda1ba43c89d843965ff3a23ff75d
Mario Sepulveda <msepulveda@mobolize.com>
2023-06-30 12:48:18 -0700

    #6040 #6041 - Fix xcode 14.x is not compatible with 13.4.1 build, due to FailedChecks not conforming to CaseIterable

--- IOSSecuritySuite/FailedChecks.swift
+++ IOSSecuritySuite/FailedChecks.swift
@@ -5,12 +5,33 @@
 //  Created by im on 06/02/23.
 //  Copyright © 2023 wregula. All rights reserved.
 //
+// swiftlint:disable trailing_whitespace
 
 import Foundation
 
 public typealias FailedCheckType = (check: FailedCheck, failMessage: String)
 
 public enum FailedCheck: CaseIterable {
+    
+    /**
+     Need to implement allCases to conform to CaseIterable when built by
+     Xcode 13.4.1, in order to be compatible with Xcode 14.x
+     */
+    public static var allCases: [FailedCheck] {
+        return [
+            .urlSchemes,
+            .existenceOfSuspiciousFiles,
+            .suspiciousFilesCanBeOpened,
+            .restrictedDirectoriesWriteable,
+            .fork,
+            .symbolicLinks,
+            .dyld,
+            .openedPorts,
+            .pSelectFlag,
+            .suspiciousObjCClasses
+        ]
+    }
+
     case urlSchemes
     case existenceOfSuspiciousFiles
     case suspiciousFilesCanBeOpened
