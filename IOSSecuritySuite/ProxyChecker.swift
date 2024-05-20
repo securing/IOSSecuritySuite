23042901c2adc1e83d9e339752d81e833600f8db
Wojciech Reguła <wojciechregula@yahoo.pl>
2024-05-20 14:32:48 +0200

    VPN detection added

--- IOSSecuritySuite/ProxyChecker.swift
+++ IOSSecuritySuite/ProxyChecker.swift
@@ -9,17 +9,39 @@
 import Foundation
 
 internal class ProxyChecker {
-  static func amIProxied() -> Bool {
-    guard let unmanagedSettings = CFNetworkCopySystemProxySettings() else {
-      return false
+    static func amIProxied(considerVPNConnectionAsProxy: Bool = false) -> Bool {
+        guard let unmanagedSettings = CFNetworkCopySystemProxySettings() else {
+            return false
+        }
+        
+        let settingsOptional = unmanagedSettings.takeRetainedValue() as? [String: Any]
+        
+        guard  let settings = settingsOptional else {
+            return false
+        }
+        
+        if(considerVPNConnectionAsProxy) {
+            if let scoped = settings["__SCOPED__"] as? [String: Any] {
+                for interface in scoped.keys {
+                    
+                    let names = [
+                        "tap",
+                        "tun",
+                        "ppp",
+                        "ipsec",
+                        "utun"
+                    ]
+                    
+                    for name in names {
+                        if(interface.contains(name)) {
+                            print("detected: \(interface)")
+                            return true
+                        }
+                    }
+                }
+            }
+        }
+        
+        return (settings.keys.contains("HTTPProxy") || settings.keys.contains("HTTPSProxy"))
     }
-    
-    let settingsOptional = unmanagedSettings.takeRetainedValue() as? [String: Any]
-    
-    guard  let settings = settingsOptional else {
-      return false
-    }
-    
-    return (settings.keys.contains("HTTPProxy") || settings.keys.contains("HTTPSProxy"))
-  }
 }
